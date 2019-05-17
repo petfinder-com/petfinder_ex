2f377b7220d6f090f1829cf308149409279dd6d6
Tim Johnson <timothy.johnson2@purina.nestle.com>
2019-05-17 13:21:36 -0400

    add organizations module

--- lib/petfinder/helpers.ex
+++ lib/petfinder/helpers.ex
@@ -1,5 +1,6 @@
 defmodule Petfinder.Helpers do
   alias Petfinder.Auth
+
   def handle_http_response({:ok, %{status_code: 200, body: body}}), do: {:ok, Poison.decode!(body)}
   def handle_http_response({:ok, %{status_code: 404}}), do: {:error, "Not found :("}
   def handle_http_response({:ok, %{reason: reason}}), do: {:error, IO.inspect reason}
