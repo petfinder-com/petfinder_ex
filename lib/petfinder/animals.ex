41bd60e7316813306f6749c981ad709708e1252c
Nolan Davidson <nolan.davidson@purina.nestle.com>
2020-05-14 08:46:28 -0400

    Updating for Elixir 1.10
    Looking for app configs at compile time no longer works properly when
    being used as a dep. Moving those configs to runtime functions.

--- lib/petfinder/animals.ex
+++ lib/petfinder/animals.ex
@@ -1,32 +1,28 @@
 defmodule Petfinder.Animals do
   alias Petfinder.Helpers
 
-  @base_url Application.fetch_env!(:petfinder, :base_url)
-
   def get_animals(params \\ %{}) do
-    "#{@base_url}/v2/animals"
+    "#{Helpers.base_url()}/v2/animals"
     |> Helpers.process_get(params)
   end
 
   def get_animal_by_id(id) do
-    "#{@base_url}/v2/animals/#{id}"
+    "#{Helpers.base_url()}/v2/animals/#{id}"
     |> Helpers.process_get()
   end
 
   def get_animal_types() do
-    "#{@base_url}/v2/types"
+    "#{Helpers.base_url()}/v2/types"
     |> Helpers.process_get()
   end
 
   def get_animal_type_by_name(type_name) do
-    "#{@base_url}/v2/types/#{type_name}"
+    "#{Helpers.base_url()}/v2/types/#{type_name}"
     |> Helpers.process_get()
   end
 
-
   def get_animal_breeds_for_type(type_name) do
-    "#{@base_url}/v2/types/#{type_name}/breeds"
+    "#{Helpers.base_url()}/v2/types/#{type_name}/breeds"
     |> Helpers.process_get()
   end
 end
-
--- lib/petfinder/organizations.ex
+++ lib/petfinder/organizations.ex
@@ -1,15 +1,13 @@
 defmodule Petfinder.Organizations do
   alias Petfinder.Helpers
 
-  @base_url Application.fetch_env!(:petfinder, :base_url)
-
   def get_organizations(params \\ %{}) do
-    "#{@base_url}/v2/organizations"
+    "#{Helpers.base_url()}/v2/organizations"
     |> Helpers.process_get(params)
   end
 
   def get_organization_by_id(org_id) do
-    "#{@base_url}/v2/organizations/#{org_id}"
+    "#{Helpers.base_url()}/v2/organizations/#{org_id}"
     |> Helpers.process_get()
   end
 end
